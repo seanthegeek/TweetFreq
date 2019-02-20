af758864226f54dedb9cfe2c2365404044252563
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2019-02-20 09:10:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 pkill gunicorn > /dev/null 2>&1
 pkill celery > /dev/null 2>&1
  
-/usr/local/bin/gunicorn --reload -D -w 4 -b 127.0.0.1:8000 tweetfreq:APP 
+gunicorn --reload -D -w 4 -b 127.0.0.1:8000 tweetfreq:APP
 celery worker --config=config > /dev/null 2>&1 &
 
